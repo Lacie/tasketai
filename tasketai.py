ab1091831f2862a72882c9264f80a340971ccf0e
lacie <lacie@lacie.dev>
2025-02-11 16:05:35 -0800

    loading and ranking task test

--- tasketai.py
+++ tasketai.py
@@ -4,7 +4,7 @@ Description: Main runner file.
 
 Author: Lacie Turner
 Date created: 2025-02-10
-Date last modified: 2025-02-10
+Date last modified: 2025-02-11
 Python Version: 3.12
 """
 
@@ -15,13 +15,19 @@ __credits__ = ["Lacie Turner"]
 __status__ = "Development"
 __version__ = "0.0.1"
 
+from src.rank_tasks import rank_tasks, print_dataframe
+from src.task import load_tasks_from_csv
 
 def get_daily_velocity() -> int:
 	pass
 
 
 def main():
-	pass
+	task_df = load_tasks_from_csv()
+	ranked_df = rank_tasks(task_df)
+
+	print_dataframe(ranked_df)
+
 
 
 if __name__ == '__main__':
