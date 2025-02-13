f42f1e655f808bf697e3916c3c3ec83073348497
Lacie <lacie@lacie.dev>
2025-02-12 21:35:45 -0800

    started fleshing out functionality

--- src/task.py
+++ src/task.py
@@ -4,7 +4,7 @@ Description: Defines the Task object.
 
 Author: Lacie Turner
 Date created: 2025-02-10
-Date last modified: 2025-02-11
+Date last modified: 2025-02-12
 Python Version: 3.12
 """
 
@@ -17,9 +17,8 @@ __version__ = "0.0.1"
 
 import uuid
 import pandas as pd
+import datetime as dt
 from pathlib import Path
-
-CSV_FILE = Path("./data/generated-test-tasks.csv")
 
 
 class Task:
@@ -27,10 +26,27 @@ class Task:
 		pass
 
 
-def load_tasks_from_csv(csv_file: Path = CSV_FILE) -> pd.DataFrame:
-	task_df = pd.read_csv(csv_file)
+def load(task_file: Path) -> pd.DataFrame:
+	"""Loads a task file into a pandas DataFrame."""
+	assert task_file.exists(), f"Task file does not exist at {task_file}"
+	task_df = pd.read_csv(task_file)
 
 	return task_df
+
+
+def is_valid_title(title: str) -> bool:
+	if not title.strip() or len(title.strip()) > 256:
+		return False
+	return True
+
+
+def add(title: str, urgency: int, importance: int, effort: int, csv_file: Path) -> None:
+	if csv_file.exists() and csv_file.stat().st_size > 0:
+		with open(csv_file, "a") as f:
+			now = dt.datetime.now().isoformat()
+			#id,complete,title,urgency,importance,effort,due_date,_created,_modified
+			f.write(f"{uuid.uuid4()},FALSE,{title},{urgency},{importance},{effort},,{now},{now}\n")
+		print(f"\nTask added!\n")
 
 
 def get_task_by_id(_id: uuid.UUID):
@@ -49,5 +65,31 @@ def get_incomplete_tasks():
 	pass
 
 
-def get_task_suggestions(velocity: int):
+def get_task_suggestions(velocity: int, ranked_dataframe: pd.DataFrame) -> pd.DataFrame:
+	assert velocity in range(1, 3), "Velocity must be between 1 and 3"
 	pass
+
+
+def display_list(task_dataframe, start_index: int=0, end_index: int=-1) -> None:
+	task_df = task_dataframe[start_index:end_index]
+	print(f"\n{'Task'.ljust(48)}"
+	      f"{'Complete'.ljust(12)}"
+	      f"{'Urgency'.ljust(11)}"
+	      f"{'Importance'.ljust(14)}"
+	      f"{'Effort'.ljust(10)}"
+	      f"{'Due Date'.ljust(20)}")
+	print("-" * 114)
+	for index, row in task_df.iterrows():
+		print(f"{row['title'].ljust(48)}"
+		      f"{'Y'.ljust(12) if row['complete'] == "FALSE" else 'N'.ljust(12)}"
+		      f"{str(row['urgency']).ljust(11)}"
+		      f"{str(row['importance']).ljust(14)}"
+		      f"{str(row['effort']).ljust(10)}"
+		      f"{str(row['due_date']).ljust(20) if row['due_date'] == row['due_date'] else '--'.ljust(20)}")
+	print()
+
+
+def display(task_id: uuid, task_csv: Path) -> None:
+	task_dataframe = load(task_csv)
+	task = task_dataframe.loc[task_dataframe['id'] == task_id]
+	print(task)
--- tasketai.py
+++ tasketai.py
@@ -4,7 +4,7 @@ Description: Main runner file.
 
 Author: Lacie Turner
 Date created: 2025-02-10
-Date last modified: 2025-02-11
+Date last modified: 2025-02-12
 Python Version: 3.12
 """
 
@@ -15,19 +15,191 @@ __credits__ = ["Lacie Turner"]
 __status__ = "Development"
 __version__ = "0.0.1"
 
-from src.rank_tasks import rank_tasks, print_dataframe
-from src.task import load_tasks_from_csv
+import inquirer
+import src.task as task
+from inquirer import errors
+import src.user_data as user_data
+from src.user_data import USER_TASK_FILE
 
-def get_daily_velocity() -> int:
+MAX_TASKS_PER_PAGE = 20
+
+
+def print_header():
+	"""Prints the program header."""
+	print("*" *  50)
+	print(f"*{'Welcome to Tasketai!'.center(48, ' ')}*")
+	print("*" * 50)
+
+
+def task_input_validation(answers, current):
+	"""
+	Validates the task input.
+
+	:param answers: The list of answers.
+	:param current: The current answer.
+	:raise: ValidationError
+	"""
+	title = answers.get("title", current)
+	urgency = current if answers.get('title') else 1
+	importance = current if answers.get('urgency') else 1
+	effort = current if answers.get('importance') else 1
+	try:
+		assert task.is_valid_title(title), "Must be between 1 and 256 characters long"
+		assert int(urgency) in range(1, 6), "Must be between 1 and 5"
+		assert int(importance) in range(1, 6), "Must be between 1 and 5"
+		assert int(effort) in range(1, 4), "Must be between 1 and 3"
+	except AssertionError as err:
+		raise errors.ValidationError("", reason=str(err))
+	except KeyboardInterrupt:
+		raise
+	return True
+
+def velocity_input_validation(answers, current):
+	"""
+	Validates the velocity input.
+
+	:param answers: The list of answers.
+	:param current: The current answer.
+	:raise: ValidationError
+	"""
+	velocity = answers.get("velocity", current)
+	try:
+		assert int(velocity) in range(1, 4), "Must be between 1 and 3"
+	except AssertionError as err:
+		raise errors.ValidationError("", reason=str(err))
+	except KeyboardInterrupt:
+		raise
+	return True
+
+
+def view_task_list() -> None:
+	"""Paginated view of task list."""
+	task_df = task.load(USER_TASK_FILE)
+	# print(f"[DEBUG] Loaded {len(task_df)} tasks")
+
+	start = 0
+	end = MAX_TASKS_PER_PAGE if len(task_df) > MAX_TASKS_PER_PAGE else -1
+	load_more = True if len(task_df) > MAX_TASKS_PER_PAGE else False
+	while True:
+		task.display_list(task_df, start, end)
+		if end >= len(task_df):
+			load_more = False
+		try:
+			choice = inquirer.list_input("Select an option", choices=["See more", "Back"] if load_more else ["Back"])
+			if choice == "Back":
+				break
+			elif choice == "See more":
+				start = end
+				end = start + MAX_TASKS_PER_PAGE
+
+		except KeyboardInterrupt:
+			break
+
+
+def add_task_input() -> None:
+	"""Prompts the user for the task to add."""
+	try:
+		questions = [
+			inquirer.Text(f"title",
+						  message=f"Title",
+						  validate=task_input_validation),
+			inquirer.Text("urgency",
+						  message="Urgency",
+						  validate=task_input_validation),
+			inquirer.Text("importance",
+						  message="Importance",
+						  validate=task_input_validation),
+			inquirer.Text("effort",
+						  message="Effort",
+						  validate=task_input_validation)
+		]
+
+		answers = inquirer.prompt(questions)
+		if not answers:
+			return
+		task.add(title=answers.get('title'),
+				 urgency=answers.get('urgency'),
+				 importance=answers.get('importance'),
+				 effort=answers.get('effort'),
+				 csv_file=USER_TASK_FILE)
+
+	except (TypeError, KeyboardInterrupt):
+		return
+
+
+def velocity_input() -> None:
+	"""Prompts the user for their velocity."""
+	choices = ["None (I have no wiggle room today)",
+	           "Low (I have a lot on my plate, but I can fit something small in)",
+	           "Medium (I can  definitely tackle a task or two)",
+	           "High (My day is wide open!)",
+	           "Back"
+	           ]
+
+	while True:
+		try:
+			choice = inquirer.list_input("Select your velocity for today",
+			                             choices=choices
+			                             )
+			if choice == choices[-1]:
+				break
+
+			user_data.add_velocity(choice.index(choice))
+			break
+
+		except KeyboardInterrupt:
+			break
+
+
+def suggest_task_menu() -> None:
+	"""Prompts the user to select suggested tasks."""
 	pass
 
 
+def main_menu():
+	"""Provides the main menu."""
+	user_data.greeting()
+	while True:
+		user_data.display()
+		choices = []
+		if not user_data.has_attr('velocity'):
+			choices.append("Velocity")
+		if not user_data.has_attr('selected_tasks'):
+			choices.append("Suggest Tasks")
+
+		try:
+			choice = inquirer.list_input("Select an option",
+										 choices= choices + [
+											"Add Task",
+											"View Task Backlog",
+											"Quit"
+											]
+										 )
+			if choice == "Quit":
+				break
+			elif choice == "Add Task":
+				add_task_input()
+			elif choice == "View Task Backlog":
+				view_task_list()
+			elif choice == "Velocity":
+				velocity_input()
+			elif choice == "Suggest Tasks":
+				suggest_task_menu()
+
+		except KeyboardInterrupt:
+			break
+
+	print("Goodbye!")
+
+
 def main():
-	task_df = load_tasks_from_csv()
-	ranked_df = rank_tasks(task_df)
+	print_header()
+	# is_returning_today = dt.datetime.fromisoformat(data.get('last_login')).date() == dt.datetime.now().date()
+	main_menu()
 
-	print_dataframe(ranked_df)
-
+	# task_df = load_tasks_from_csv()
+	# ranked_df = rank_tasks(task_df)
+	# print_dataframe(ranked_df)
 
 
 if __name__ == '__main__':
