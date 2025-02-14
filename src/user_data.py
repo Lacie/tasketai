e053c17f0649e95fbe0def2a71aacd5d1b7b700d
lacie <lacie@lacie.dev>
2025-02-13 18:30:11 -0800

    Updated docstrings

--- src/user_data.py
+++ src/user_data.py
@@ -25,7 +25,12 @@ USER_TASK_FILE = Path("./data/user-tasks.csv").resolve()
 USER_DATA_SCHEMA_FILE = Path("./data/__user-data-schema.json").resolve()
 
 
-def load():
+def load() -> dict:
+	"""
+	Loads the user's data from a JSON file.
+
+	:return: The loaded user's data.
+	"""
 	if not USER_DATA_FILE.exists():
 		print(f"[DEBUG] No user data file found")
 		USER_DATA_FILE.touch()
@@ -46,17 +51,30 @@ def load():
 
 
 def greeting():
+	"""Displays a greeting to the user."""
 	user_data = load()
 	print(f"\nHello{f', {user_data.get('username')}' if user_data.get('username') else ''}!\n")
 
 
 def has_attr(attribute: str) -> bool:
+	"""
+	Checks if a user's data has the given attribute.
+
+	:param attribute: The attribute to check.
+	:return: Whether the user has the given attribute.
+	"""
 	user_data = load()
 	today = str(dt.date.today())
 	return False if user_data.get(attribute).get(today) is None else True
 
 
 def get_attr(attribute: str):
+	"""
+	Gets the specified attribute from the user's data.
+
+	:param attribute: The attribute to get.
+	:return: The specified attribute, if it exists, otherwise None.
+	"""
 	user_data = load()
 	if attribute in ['velocity', 'selected_tasks', 'completed_tasks', 'suggested_tasks', 'rejected_tasks']:
 		today = str(dt.date.today())
@@ -65,17 +83,27 @@ def get_attr(attribute: str):
 
 
 def add_username(username: str):
+	"""
+	Adds a username to the user's data file. Will also greet the user again with their new username.
+
+	:param username: The username to add.
+	"""
 	user_data = load()
 	user_data["username"] = username
 	USER_DATA_FILE.write_text(json.dumps(user_data))
 	print(f"\n[INFO] Successfully {f'updated username to \'{username}\'' if username else 'removed username'}.")
 
-	# re-greet them by the new name :)
+	# re-greet them by their new name :)
 	if username:
 		greeting()
 
 
 def add_velocity(velocity: int):
+	"""
+	Adds a velocity to the user data file.
+
+	:param velocity: The velocity to add.
+	"""
 	user_data = load()
 	today = str(dt.date.today())
 	user_data["velocity"][today] = velocity
@@ -84,6 +112,11 @@ def add_velocity(velocity: int):
 
 
 def get_selected_effort() -> int:
+	"""
+	Gets the cumulative effort of the user's selected tasks.
+
+	:return: The cumulated effort of the user's currently selected tasks.
+	"""
 	user_data = load()
 	selected_tasks = get_attr('selected_tasks')
 
@@ -94,33 +127,48 @@ def get_selected_effort() -> int:
 	return selected_effort
 
 
-def add_task(list_type: str, task_id: str):
-	assert list_type in ["completed", "suggested", "rejected", "selected"], "Invalid list type"
+def add_task(task_list: str, task_id: str):
+	"""
+	Adds a task to the user data file.
+
+	:param task_list: The list to add the task to. Must be of one of the following: ["completed", "suggested", "rejected", "selected"]
+	:param task_id: The task ID to add.
+	:raises: AssertionError if the task_list is not one of the following: ["completed", "suggested", "rejected", "selected"]
+	"""
+	assert task_list in ["completed", "suggested", "rejected", "selected"], "Invalid list type"
 	user_data = load()
 	today = str(dt.date.today())
 
-	if not user_data[f"{list_type}_tasks"].get(today):
-		user_data[f"{list_type}_tasks"][today] = []
+	if not user_data[f"{task_list}_tasks"].get(today):
+		user_data[f"{task_list}_tasks"][today] = []
 
-	if task_id not in user_data[f"{list_type}_tasks"][today]:
-		user_data[f"{list_type}_tasks"][today].append(task_id)
+	if task_id not in user_data[f"{task_list}_tasks"][today]:
+		user_data[f"{task_list}_tasks"][today].append(task_id)
 
 	USER_DATA_FILE.write_text(json.dumps(user_data))
-	print(f"[DEBUG] Added task {task_id} to {list_type}_tasks.")
+	# print(f"[DEBUG] Added task {task_id} to {task_list}_tasks.")
 
 
-def del_task(list_type: str, task_id: str):
-	assert list_type in ["completed", "suggested", "rejected", "selected"], "Invalid list type"
+def del_task(task_list: str, task_id: str):
+	"""
+	Adds a task to the user data file.
+
+	:param task_list: The list to add the task to. Must be of one of the following: ["completed", "suggested", "rejected", "selected"]
+	:param task_id: The task ID to add.
+	:raises: AssertionError if the task_list is not one of the following: ["completed", "suggested", "rejected", "selected"]
+	"""
+	assert task_list in ["completed", "suggested", "rejected", "selected"], "Invalid list type"
 	user_data = load()
 	today = str(dt.date.today())
 
-	if task_id in user_data[f"{list_type}_tasks"][today]:
-		user_data[f"{list_type}_tasks"][today].remove(task_id)
+	if task_id in user_data[f"{task_list}_tasks"][today]:
+		user_data[f"{task_list}_tasks"][today].remove(task_id)
 
 	USER_DATA_FILE.write_text(json.dumps(user_data))
 
 
-def display() -> None:
+def display_message():
+	"""Displays messages depending on the current state of the user's data."""
 	user_data = load()
 	today = str(dt.date.today())
 	velocity = user_data.get("velocity").get(today)
